4a44f83e4042dd5a27d08257aa9c613de89fb367
Ticha974 <natacha.crepu@hotmail.fr>
2022-01-27 16:11:47 +0100

    CSS form

--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -6,6 +6,7 @@ use App\Repository\QuestionRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=QuestionRepository::class)
@@ -21,11 +22,18 @@ class Question
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Length(
+     *      min = 3,
+     *      max = 255,
+     *      minMessage = "Your title must be at least {{ limit }} characters long",
+     *      maxMessage = "Your title name cannot be longer than {{ limit }} characters")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank
      */
     private $content;
 
@@ -47,6 +55,7 @@ class Question
 
     /**
      * @ORM\ManyToMany(targetEntity=Tag::class, mappedBy="questions")
+     * @ORM\JoinTable(name="tag_question")
      */
     private $tags;
 
@@ -56,7 +65,7 @@ class Question
         $this->tags = new ArrayCollection();
     }
 
-    public function getId(): ?int
+    public function getId(): int
     {
         return $this->id;
     }
