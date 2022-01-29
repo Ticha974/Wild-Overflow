c7055365717e6462731c5f979bfefb7e816597bd
Ticha974 <natacha.crepu@hotmail.fr>
2022-01-29 01:22:48 +0100

    add use Constraints in ConctactType

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -35,7 +35,7 @@ class ContactController extends AbstractController
             return $this->redirectToRoute('contact');
         }
         return $this->render('contact/index.html.twig', [
-            'our_form' => $form->createView()
+            'form' => $form->createView()
         ]);
     }
 }
