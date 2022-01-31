6e6dbf23db7d02ff7a2766230f12cfd14d5fb141
kiran <kiran.deshmukh111@gmail.com>
2022-01-31 17:25:59 +0100

    changes in question controller

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -13,7 +13,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 
-use function Amp\Iterator\fromIterable;
 
 /**
  * @Route("/question", name="question_")
